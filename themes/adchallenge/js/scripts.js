548b2d26d2adfbf85541179967b0abf7ee5b027d
GoodGuyRobert <93642293+GoodGuyRobert@users.noreply.github.com>
2021-11-10 09:23:44 +0100

    Add files via upload

---
+++ themes/adchallenge/js/scripts.js
@@ -0,0 +1,45 @@
+
+(function ($, Drupal) {
+    $(document).ready(function () {
+        $('a[href^="#"]').bind('click', function (e) {
+            e.preventDefault(); // prepreči skok
+
+            var target = $(this).attr("href"); // Nastavi cilj kot spremenljivko 
+
+            // izvede animirano pomikanje tako, da dobi zgornji položaj ciljnega elementa in ga nastavite kot cilj pomikanja 
+            $('html, body').stop().animate({
+                scrollTop: $(target).offset().top
+            }, 600, function () {
+                location.hash = target; //pripne hash (#jumptarget) na pageurl 
+            });
+
+            return false;
+        });
+    });
+
+    var addClassOnScroll = function () {
+        var windowTop = $(window).scrollTop();
+        $('div[id]').each(function (index, elem) {
+            var offsetTop = $(elem).offset().top;
+            var outerHeight = $(this).outerHeight(true);
+
+            if (windowTop > (offsetTop - 200) && windowTop < (offsetTop + outerHeight)) {
+                var elemId = $(elem).attr('id');
+
+
+
+                $("header div nav ul li a.active").removeClass('active');
+                $("header div nav ul li a[href='#" + elemId + "']").addClass('active');
+
+            }
+        });
+    };
+
+    $(function () {
+        $(window).on('scroll', function () {
+            addClassOnScroll();
+        });
+    });
+
+
+})(jQuery, Drupal);
